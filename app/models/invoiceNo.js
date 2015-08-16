93fdd974f600b9c85ab6b45b159547c05f47ed99
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2015-08-16 13:59:07 +0200

    model for keeping invoice number

---
+++ app/models/invoiceNo.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var InvoiceNoSchema = new Schema({
+	year: Number,
+	month: Number,
+	no: Number
+});
+
+
+module.exports = mongoose.model('invoiceno', InvoiceNoSchema);
