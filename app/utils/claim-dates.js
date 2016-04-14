ea9e314d222c812d47a59ebb872d418bed2b3290
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2016-04-14 22:38:44 +0200

    [fix] Calculate claim end date only when status === 'creatingPayment'

--- app/utils/claim-dates.js
+++ app/utils/claim-dates.js
@@ -6,13 +6,23 @@ module.exports = function (claim) {
     var timeToPayInSeconds = daysToPay * 3600 * 24;
 
     var eventDate = moment(claim.event.eventStartDate);
-    var endDate = moment(new Date(Date.now() + 1000 * timeToPayInSeconds));
+    var endDate;
 
-//polnoc z czwartku na piatek to ostatni moment na dokonanie oplaty po tym czasie rezerwacja jest kasowana
-    var lastDateToPay = eventDate.clone().subtract(1, 'days').hour(0).minute(0).second(0);
-    if (lastDateToPay.isBefore(endDate)) {
-        endDate = lastDateToPay;
+    if (claim.status === 'creatingPayment') {
+
+        endDate = moment(new Date(Date.now() + 1000 * timeToPayInSeconds));
+
+        //polnoc z czwartku na piatek to ostatni moment na dokonanie oplaty po tym czasie rezerwacja jest kasowana
+        var lastDateToPay = eventDate.clone().subtract(1, 'days').hour(0).minute(0).second(0);
+        if (lastDateToPay.isBefore(endDate)) {
+            endDate = lastDateToPay;
+        }
     }
+    else {
+        endDate = moment(claim.validTill);
+    }
+
+
 
     return {
       endDate: endDate,
