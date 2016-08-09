ca93607f3c75d62ffde3693dd2812b03df29e62a
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2016-08-10 00:18:01 +0200

    [feat] Dodaj API do dodawania/aktualizowania eventu

--- app/controllers/api/events.js
+++ app/controllers/api/events.js
@@ -3,8 +3,25 @@ var errors = require('domain').create();
 
 module.exports = function (router) {
   router.post('/events', function (req, res) {
-    Event.create(req.body, errors.intercept(function (ev) {
-      res.send(ev);
-    }));
+    req.body.ticketsLeft = req.body.ticketsLeft || req.body.tickets;
+
+    Event.findOne({
+      name: req.body.name
+    }).exec(function (err, event){
+      if (event) {
+        Event.update({
+          _id: event._id
+        },req.body, errors.intercept(function (ev) {
+          res.send(ev);
+        }));
+      } else {
+        Event.create(req.body, errors.intercept(function (ev) {
+          res.send(ev);
+        }));
+      }
+    });
+
+
   });
+
 };
