ca05921d57bfa952e6bd757b8890a8314d2b327b
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2016-08-18 11:17:55 +0200

    [feat] Dodaj weryfikacje czy przekazany w parametrach kraj istnieje

--- app/controllers/api/events.js
+++ app/controllers/api/events.js
@@ -1,27 +1,38 @@
 var Event = require('../../models/event');
+var Country = require('../../models/country');
 var errors = require('domain').create();
 
 module.exports = function (router) {
-  router.post('/events', function (req, res) {
+  router.post('/events', function (req, res, next) {
+        Country.findOne({
+          name: req.body.country.toLowerCase()
+        }).exec(function (err, country) {
+          if (err || !country) {
+            res.status(404).send();
+            return;
+          }
+
+          req.body.country = country._id;
+          next();
+        });
+    }, function (req, res) {
     req.body.ticketsLeft = req.body.ticketsLeft || req.body.tickets;
 
-    Event.findOne({
-      name: req.body.name
-    }).exec(function (err, event){
-      if (event) {
-        Event.update({
-          _id: event._id
-        },req.body, errors.intercept(function (ev) {
-          res.send(ev);
-        }));
-      } else {
-        Event.create(req.body, errors.intercept(function (ev) {
-          res.send(ev);
-        }));
-      }
-    });
-
-
+      Event.findOne({
+        name: req.body.name
+      }).exec(function (err, event){
+        if (event) {
+          Event.update({
+            _id: event._id
+          },req.body, errors.intercept(function (ev) {
+            res.send(ev);
+          }));
+        } else {
+          Event.create(req.body, errors.intercept(function (ev) {
+            res.send(ev);
+          }));
+        }
+      });
   });
 
   router.get('/event/:name/tickets-left', function (req, res) {
