47ab0247781906cdab8e384d9ddc81b182a51037
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2016-08-19 08:55:37 +0200

    [lang] Dodaj nowe tlumaczenia dla rejestracji

--- app/utils/ticket_translations.js
+++ app/utils/ticket_translations.js
@@ -2,12 +2,21 @@
 
 module.exports = function (translator) {
     var result = {
-        ticket: {
-            alert: {}
-        }
+        alert: {},
+        payment: {}
     };
-    result.ticket.alert.header = translator('ticket.alert.header');
-    result.ticket.alert.paragraph = translator('ticket.alert.paragraph');
+    result.alert.header = translator('ticket.alert.header');
+    result.alert.paragraph = translator('ticket.alert.paragraph');
+    result.alert.timeout = translator('ticket.alert.timeout');
+    result.alert.timeoutInfoPart1 = translator('ticket.alert.timeoutInfoPart1');
+    result.alert.timeoutInfoPart1 = translator('ticket.alert.timeoutInfoPart2');
+    result.title = translator('ticket.title');
+    result.mail = translator('ticket.mail');
+    result.names = translator('ticket.names');
+    result.btnConfirm = translator('ticket.btnConfirm');
+    result.payment.title = translator('ticket.payment.title');
+    result.payment.declaredAmount = translator('ticket.payment.declaredAmount');
+    result.payment.diplomaInfo = translator('ticket.payment.diplomaInfo');
 
     return result;
 };
