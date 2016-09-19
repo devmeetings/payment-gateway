37c4fafd50ec6adaa6d4f368f1a89f83144b3531
Łukasz Szewczak <lukaszewczak@gmail.com>
2016-09-20 00:39:41 +0200

    [feat] Dodaj modul do pobrania i renderowania maili

---
+++ app/utils/mail-renderer.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var request = require('request');
+var utf8Converter = require('./utf8-converter');
+var config = require('../../config/config');
+
+module.exports = function (mailPath, lang, variables, cb) {
+    request(config.mailTemplateURL + mailPath + '?lang=' + lang, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var mailText = response.body;
+            mailText = mailText.replace(/^[\s\uFEFF\xA0"]+|[\s\uFEFF\xA0"]+$/g, '');
+            mailText = mailText.replace(/\\"/g, '"').replace(/\\\//g, '/').replace(/\\n/g, '');
+            mailText = utf8Converter(mailText);
+
+            Object.keys(variables).forEach(function (key) {
+                var reg;
+                var condititionalKeys = ['IF_INVOICE', 'IF_PAYU'];
+                var specialKeys = ['LINK'];
+                var foundKey = false;
+
+                condititionalKeys.forEach(function (condititionalKey) {
+                    if (key === condititionalKey && variables[key] === true) {
+                        reg = new RegExp("%" + key + '%', "g");
+                        mailText = mailText.replace(reg, '');
+                        foundKey = true;
+                    } else if (key === condititionalKey && variables[key] === false) {
+                        reg = new RegExp("%"+condititionalKey+"%(\w|\W)*%"+condititionalKey+"%");
+                        mailText = mailText.replace(reg, '');
+                        foundKey = true;
+                    }
+                });
+
+                specialKeys.forEach(function (condititionalKey) {
+                    if (key === condititionalKey && key === 'LINK') {
+                        reg = new RegExp('%LINK%', "g");
+                        mailText = mailText.replace(reg, '<a href="' + variables[key]+ '">');
+                        reg = new RegExp('%END_LINK%', "g");
+                        mailText = mailText.replace(reg, '')
+                        foundKey = true;
+                    }
+                });
+
+                if (!foundKey) {
+                    reg = new RegExp("%" + key + '%', "g");
+                    mailText = mailText.replace(reg, variables[key]);
+                }
+
+            });
+
+            cb(mailText);
+        }
+    });
+}
