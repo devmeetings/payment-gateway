fc54dd7e85ec7837454c5c261c103a720d7f7e78
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-06-28 00:18:46 +0200

    [chore] Set up eventbrite token

--- config/config.js
+++ config/config.js
@@ -5,46 +5,47 @@ var env = process.env.NODE_ENV || 'development';
 var languages = ['pl', 'en', 'de'];
 
 var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'payment-gateway',
-      url: 'http://localhost:3000'
+    development: {
+        root: rootPath,
+        app: {
+            name: 'payment-gateway',
+            url: 'http://localhost:3000'
+        },
+        payu: null,
+        port: 3000,
+        db: 'mongodb://localhost/payment-gateway-development',
+        sentryDsn: false
     },
-    payu: null,
-    port: 3000,
-    db: 'mongodb://localhost/payment-gateway-development',
-    sentryDsn: false
-  },
 
-  test: {
-    root: rootPath,
-    app: {
-      name: 'payment-gateway',
-      url: 'http://devmeetings.pl'
+    test: {
+        root: rootPath,
+        app: {
+            name: 'payment-gateway',
+            url: 'http://devmeetings.pl'
+        },
+        payu: null,
+        port: 3000,
+        db: 'mongodb://localhost/payment-gateway-staging',
+        sentryDsn: false
     },
-    payu: null,
-    port: 3000,
-    db: 'mongodb://localhost/payment-gateway-staging',
-    sentryDsn: false
-  },
 
-  production: {
-    root: rootPath,
-    app: {
-      name: 'payment-gateway',
-      url: 'http://devmeetings.com'
-    },
-    payu: {
-      id: '183152',
-      key: '8da6d279e0b48992aa164b5a431a78b7'
-    },
-    port: 3000,
-    db: 'mongodb://localhost/payment-gateway-production',
-    sentryDsn: 'https://08525662ea9f4e4a9aaaec77999ae3cb:21f7694047b84f868e44a0baa0d81b3a@app.getsentry.com/51486'
-  }
+    production: {
+        root: rootPath,
+        app: {
+            name: 'payment-gateway',
+            url: 'http://devmeetings.com'
+        },
+        payu: {
+            id: '183152',
+            key: '8da6d279e0b48992aa164b5a431a78b7'
+        },
+        port: 3000,
+        db: 'mongodb://localhost/payment-gateway-production',
+        sentryDsn: 'https://08525662ea9f4e4a9aaaec77999ae3cb:21f7694047b84f868e44a0baa0d81b3a@app.getsentry.com/51486'
+    }
 };
 
 module.exports = config[env];
+module.exports.eventbrite = 'C7GVDKSZEB76O6U7HGSR';
 module.exports.languages = languages;
 module.exports.env = env;
